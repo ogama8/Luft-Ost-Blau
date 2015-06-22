5c87ca1f7ebb60ce5068eaba8809ba2843b2fc44
Eli Backer <ogama8@gmail.com>
2015-06-22 03:01:41 -0700

    Minor changes

--- LOB.py
+++ LOB.py
@@ -7,33 +7,18 @@ Lob's API to pass this data on to them.
 Started June 2015 by Eli Backer.
 """
 
-### GLOBALS   ---------------------------------------------------------------
-DPI         = 600
-WIDTH       = int(6.25 * DPI)
-HEIGHT      = int(4.25 * DPI)
-PADDING     = int((0.25 + 0.125) * DPI)
-
-
 ### IMPORTS   ---------------------------------------------------------------
 import sys
 import math
 import PIL
 
 
-### IMAGE/TEXT HANDLING   ---------------------------------------------------
-from PIL import Image, ImageFont, ImageDraw
-TEXT_MULT = 4     # This is the scaler to get better looking text by
-                  # rendering big and then downscaling with Anti-Aliasing
-
-serif_font      = ImageFont.truetype("resources/hepworth-regular.ttf", 60 * TEXT_MULT)
-TEXT_HEIGHT     = 100
-
-# From the top of the Safe Zone to the KEEP OUT is exactly 1.25"
-# From the left of the Safe Zone to the KEEP OUT is exactly 2.25"
-NUM_FULL_LINES  = math.floor(DPI / TEXT_HEIGHT * 1.25)
-FULL_LINE_WIDTH = int(WIDTH - 2*PADDING) * TEXT_MULT
-HALF_LINE_WIDTH = int(2.25 * DPI) * TEXT_MULT
-NEW_LINE        = 0.35      # This, plus 1 will be the line spacing for a \n
+### IMAGE HANDLING   --------------------------------------------------------
+from PIL import Image
+DPI         = 600
+WIDTH       = int(6.25 * DPI)
+HEIGHT      = int(4.25 * DPI)
+PADDING     = int((0.25 + 0.125) * DPI)
 
 
 ### MAKE SSL HAPPY   --------------------------------------------------------
@@ -68,60 +53,14 @@ front_im = front_im.resize((HEIGHT, WIDTH), PIL.Image.LANCZOS)
 front_im.save('front' + '.jpg', 'JPEG')
 
 
-message = "<p>I LOOKED AT MY NOTES AND I DIDN'T LIKE THEM.  I'd spent three days at U.S. Robots and might as well have spent them at home with the Encyclopedia Tellurica.</p>" + "<p>Susan Calvin had been born in the year 1982, they said, which made her seventy-five now.  Everyone knew that. Appropriately enough, U.S. Robot and Mechanical Men, Inc. was seventy-five also, since it had been in the year of Dr. Calvin's birth that Lawrence Robertson had first taken out incorporation papers for what eventually became the strangest industrial giant in man's history. Well, everyone knew that, too.</p>" + "<p>At the age of twenty, Susan Calvin had been part of the particular Psycho-Math seminar at which Dr. Alfred Lanning of U.S. Robots had demonstrated the first mobile robot to be equipped with a voice. It was a large, clumsy unbeautiful robot, smelling of machine-oil and destined for the projected mines on Mercury. But it could speak and make sense.</p>"
-
-
-'''
-
-message_arr = message.split(' ')
-message_arr.append(' ')     # Final character gets rid of annoying edge case.
-# Additionally, this final character being a space, which is impossible after
-# the split operation gives us a nice signal value to look for.
-
-back_im = Image.new('RGB', (WIDTH * TEXT_MULT, HEIGHT * TEXT_MULT), (255, 255, 255))
-draw = ImageDraw.Draw(back_im)
-
-cur_line = 0
-while (message_arr[0] != ' '):
-   current_text = message_arr[0]
-   old_text = ''
-   line_space = 1
-
-   while (draw.textsize(current_text, serif_font)[0] <\
-         (FULL_LINE_WIDTH if cur_line < NUM_FULL_LINES else HALF_LINE_WIDTH)\
-         and message_arr[0] != ' '\
-         and '\n' not in message_arr[0]):
-      current_text += " " + message_arr[1]
-      old_text += " " + message_arr.pop(0)   # get rid of element 0
-   
-   if ('\n' in message_arr[0]):
-      old_text += " " + message_arr.pop(0)
-      line_space += NEW_LINE
-
-   draw.text((PADDING * TEXT_MULT, (PADDING + cur_line*TEXT_HEIGHT) * TEXT_MULT), old_text, (0, 0, 0), font=serif_font)
-   cur_line += line_space
-
-
-#back_im = back_im.resize((WIDTH, HEIGHT), Image.LANCZOS)
-
-back_im = back_im.rotate(90)
-back_im.save('back' + '.pdf', 'PDF')
-
-#back_im.show()
-#sys.exit(0)
-
-'''
-
-
-
-
+message = "<p>Hello World (or at least my parents)!</p>" + "<p>By the time you get this, I'm either surfing or gone to RISD.  Either way, I hope it finds you well and that the text is readable.  I'm tired.  It's 2:45a and I have to get up at 5:55a.  That's soon, so I'm keeping this short.  It probably won't even have to wrap around the address box.  This is all pretty hacked together right now.  Hopefully in the future there'll be a spell-checker and other fun things.</p>" + "<p>That's all for now, love you!</p>" + "<p>Eli&mdash;</p>"
 
 
 
 postcard_response = lob.Postcard.create(
-   description  = 'Test 8',
-   to_address   = 'adr_74a812cd49adc342',
-   from_address = 'adr_74a812cd49adc342',
+   description  = 'Tom & JL - 001',
+   to_address   = 'adr_0864417ae5dcef5a',
+   from_address = 'adr_1bfeac2bb50caf6f',
    front = open("front.jpg", 'rb'),
    back  = """
    <html>
